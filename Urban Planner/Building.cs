a9bf5b949cb14e308cff34d4afb178211e81a809
Bethany Whitworth <bethanyjwhitworth@gmail.com>
2020-08-08 13:06:19 -0500

    Add project files.

---
+++ Urban Planner/Building.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Urban_Planner
+{
+    class Building
+    {
+        private string _designer = "Bethany";
+        private DateTime _dateConstructed = new DateTime();
+        private string _address = "";
+        private string _owner = "";
+
+        public int Stories { get; set; }
+        public double Width { get; set; }
+        public double Depth { get; set; }
+        public double Volume { get; private set; }
+
+        public Building(string address)
+        {
+            _address = address;
+        }
+
+        public void Construct()
+        {
+            _dateConstructed = DateTime.Now;
+            Volume = 3 * Stories * Width * Depth;
+        }
+
+        public void Purchase(string nameOfOwner)
+        {
+            _owner = nameOfOwner;
+            Console.WriteLine(
+                $"{_address}------\n" +
+                $"Designed by {_designer}\n" +
+                $"Constructed on {_dateConstructed}\n" +
+                $"Owned by {_owner}\n" +
+                $"{Volume} cubic meters of space\n"
+                );
+        }
+    }
+}
---
+++ Urban Planner/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Urban_Planner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Building FiveOneTwo = new Building("512 Green Blvd");
+            FiveOneTwo.Width = 50;
+            FiveOneTwo.Depth = 10;
+            FiveOneTwo.Stories = 2;
+            Building EighteenHundred = new Building("1800 Mountain Road");
+            EighteenHundred.Width = 60;
+            EighteenHundred.Depth = 12;
+            EighteenHundred.Stories = 9;
+            Building OneOhOne = new Building("101 Nature Trail Bend");
+            OneOhOne.Width = 10;
+            OneOhOne.Depth = 30;
+            OneOhOne.Stories = 4;
+
+            FiveOneTwo.Construct();
+            EighteenHundred.Construct();
+            OneOhOne.Construct();
+
+            FiveOneTwo.Purchase("Fred Rogers");
+            EighteenHundred.Purchase("Anon");
+            OneOhOne.Purchase("John Wark");
+
+        }
+    }
+}
